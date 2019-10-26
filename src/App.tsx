ab0bf9e58a2994284c5e65a2beffc99a71574424
thephisics101 <thephisics101@gmail.com>
2019-10-26 22:20:29 +0300

    Initial commit (part 2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Matrix} from "./matrix";
+import "./App.scss";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Point { x: number, y: number }
+interface Dimensions { width: number, height: number }
+
+interface Transformation {
+  multiplicand: Matrix
+  additive: Matrix
+  chance: number
 }
 
-export default App;
+enum Display { SVG, Raster }
+
+interface State {
+  transformations: Transformation[]
+  offset: {scale: Dimensions, offset: Point}
+  points: Point[]
+  currentPoint: Matrix
+  startingPoint: Point
+  display: Display
+}
+
+export default class extends React.Component<{}, State> {
+  state: State = {
+    transformations: [],
+    offset: {
+      scale:  {width: 1, height: 1},
+      offset: {x: 0, y: 0}
+    },
+    points: [],
+    currentPoint: new Matrix([[0], [0]]),
+    startingPoint: {x: 0, y: 0},
+    display: Display.SVG,
+  };
+
+
+  render() {
+    const {display, offset, points, transformations} = this.state;
+    return <div className="container">
+      <div className="draw-board">
+        {(() => {
+          switch (display) {
+            case Display.Raster:
+              return <canvas/>;
+            case Display.SVG:
+              const {scale: {width: sx, height: sy}, offset: {x: ox, y: oy}} = offset;
+              return <svg>
+                <g transform={`scale(${sx} ${sy}) translate(${ox} ${oy})`}>
+                  {points.map((v, k) => <circle cx={v.x} cy={v.y} r={1} key={k}/>)}
+                </g>
+              </svg>;
+          }
+        })()}
+      </div>
+      <div className="separator"/>
+      <div className="options-panel">
+        <div className="transformations">
+          {transformations.map((v, k) => <TransformationComp key={k} data={v}/>)}
+          <div className="add-new">Add new transformation</div>
+        </div>
+        <div className="separator"/>
+        <div className="options">
+          {document.location.hash}<br/>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+interface TransformationProps {
+  data: Transformation
+}
+class TransformationComp extends React.Component<TransformationProps> {
+  render() {
+    return <div/>;
+  }
+}
---
+++ src/matrix.ts
@@ -0,0 +1,50 @@
+export class Matrix {
+  w: number = -1;
+  h: number = -1;
+  data: number[][];
+
+  constructor(matrix: number[][]) {
+    this.h = matrix.length;
+    if (this.h <= 0) throw new Error("Size should be > 0");
+    this.w = matrix[0].length;
+    if (this.w <= 0) throw new Error("Size should be > 0");
+    if (matrix.some((v) => v.length !== this.w)) throw new Error("All sub-arrays should be sam size");
+    this.data = matrix;
+  }
+
+  add = (b: Matrix): Matrix => Matrix.add(this, b);
+
+  static add = (a: Matrix, b: Matrix): Matrix => {
+    if (a.w !== b.w || a.h !== a.w) throw new Error("Size doesn't match");
+    const w = a.w, h = a.h;
+    const data: number[][] = [];
+    for (let y = 0; y < h; y++) {
+      data[y] = [];
+      for (let x = 0; x < w; x++) {
+        data[y][x] = a.data[x][y] + b.data[x][y];
+      }
+    }
+    return new Matrix(data);
+  };
+
+  mult = (b: Matrix): Matrix => Matrix.mult(this, b);
+
+  static mult = (a: Matrix, b: Matrix): Matrix => {
+    if (a.w !== b.h) throw new Error("Size doesn't match");
+    const n = a.h, m = a.w, p = b.w;
+    const data: number[][] = [];
+    for (let i = 0; i < n; i++) {
+      data[i] = [];
+      for (let j = 0; j < p; j++) {
+        for (let k = 0; k < m; k++) {
+          data[i][j] += a.data[i][k] * b.data[k][j];
+        }
+      }
+    }
+    return new Matrix(data);
+  };
+
+  toString(): string {
+    return `[${this.data.map(r => `[${r.join(",")}]`)}]`
+  }
+}
