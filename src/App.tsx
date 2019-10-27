a62f541db2c9b354fc33612f7d738362a98fa47d
thephisics101 <thephisics101@gmail.com>
2019-10-27 23:43:42 +0200

    Initial commit (part 3)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ interface Transformation {
   multiplicand: Matrix
   additive: Matrix
   chance: number
+  name?: string
 }
 
 enum Display { SVG, Raster }
@@ -20,11 +21,12 @@ interface State {
   currentPoint: Matrix
   startingPoint: Point
   display: Display
+  maxChance: number
 }
 
 export default class extends React.Component<{}, State> {
   state: State = {
-    transformations: [],
+    transformations: [{multiplicand: new Matrix([[1, 2], [3, 4]]), additive: new Matrix([[1], [2]]), chance: 100}],
     offset: {
       scale:  {width: 1, height: 1},
       offset: {x: 0, y: 0}
@@ -33,11 +35,13 @@ export default class extends React.Component<{}, State> {
     currentPoint: new Matrix([[0], [0]]),
     startingPoint: {x: 0, y: 0},
     display: Display.SVG,
+    maxChance: 100
   };
 
+  changeTransformations = (k: number) => (t: Transformation) => this.setState(s => {s.transformations[k] = t; return s});
 
   render() {
-    const {display, offset, points, transformations} = this.state;
+    const {display, offset, points, transformations, maxChance} = this.state;
     return <div className="container">
       <div className="draw-board">
         {(() => {
@@ -57,7 +61,7 @@ export default class extends React.Component<{}, State> {
       <div className="separator"/>
       <div className="options-panel">
         <div className="transformations">
-          {transformations.map((v, k) => <TransformationComp key={k} data={v}/>)}
+          {transformations.map((v, k) => <TransformationComp key={k} data={v} id={k} max={maxChance} onChange={this.changeTransformations(k)}/>)}
           <div className="add-new">Add new transformation</div>
         </div>
         <div className="separator"/>
@@ -71,9 +75,59 @@ export default class extends React.Component<{}, State> {
 
 interface TransformationProps {
   data: Transformation
+  id: number
+  max: number
+  onChange(v: Transformation): any
 }
 class TransformationComp extends React.Component<TransformationProps> {
+  changeChance = ({target: {value: v}}: {target: HTMLInputElement}) => {
+    const {data, onChange} = this.props;
+    data.chance = parseFloat(v);
+    onChange(data);
+  };
+  changeMult = (x: number, y: number) => ({target: {value: v}}: {target: HTMLInputElement}) => {
+    const {data, onChange} = this.props;
+    data.multiplicand.data[x][y] = parseFloat(v);
+    onChange(data);
+  };
+  changeAdd = (x: number) => ({target: {value: v}}: {target: HTMLInputElement}) => {
+    const {data, onChange} = this.props;
+    data.additive.data[x][0] = parseFloat(v);
+    onChange(data);
+  };
+  changeName = ({target: {value: v}}: {target: HTMLInputElement}) => {
+    const {data, onChange} = this.props;
+    data.name = v;
+    onChange(data);
+  };
+
   render() {
-    return <div/>;
+    const {data, id, max} = this.props;
+    return <div>
+      <div>
+        <div>
+          <input type="text" value={data.name || `Transformation #${id+1}`} onChange={this.changeName}/>
+        </div>
+        <div>
+          <input type="number" value={data.chance} onChange={this.changeChance}/> / {max}
+        </div>
+      </div>
+      <div className="matrix">
+        {data.multiplicand.data.map((v, x) => <div key={x}>
+          {v.map((v, y) => <input type="number" key={y} value={v} onChange={this.changeMult(x, y)}/>)}
+        </div>)}
+      </div>
+      <div className="matrix"><div>x</div><div>y</div></div>
+      <div>+</div>
+      <div className="matrix">
+        {data.additive.data.map((v, x) => <div key={x}><input type="number" value={v[0]} onChange={this.changeAdd(x)}/></div>)}
+      </div>
+    </div>;
+  }
+}
+
+class ScrollableNumberInput extends React.Component<{[x: string]: any}> {
+  render() {
+    return <input {...this.props}/>
   }
 }
